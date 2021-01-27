c1ae8d4f7b9cfbeb736c7679a00365cf74dfb598
mrsdo <me@martidolce.com>
2021-01-27 06:46:11 -0500

    commit tree bce4bfc6d4f5ce05c5213f9afbb7cc9fb26600d9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,12 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def new
-    @post = Post.new
   end
 
-  # add create method here
+  def create
+
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
 end
